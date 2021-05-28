95ba15c26a55ccd79965ea61996e9ca7c4bcee50
Antonio Salvati <antonio.salvati@skillbill.it>
2021-05-28 12:14:24 +0200

    Revert "added video player"
    This reverts commit fa376dcdc7b3f3e7a0c3e1a2fa43c44c08b78f59.

--- frontend/src/components/TVStream.vue
+++ frontend/src/components/TVStream.vue
@@ -1,25 +1,13 @@
 <template>
-  <video autoplay ref="video"></video>
+  <div></div>
 </template>
 
 <script>
-import shaka from "shaka-player";
-
 export default {
-  mounted() {
-    shaka.polyfill.installAll();
 
-    const video = this.$refs.video;
-    const player = new shaka.Player(video);
-
-    player.load('https://storage.googleapis.com/shaka-demo-assets/sintel/dash.mpd');
-  }
 }
 </script>
 
 <style>
-video {
-  height: 100%;
-  width: 100%;
-}
+
 </style>
