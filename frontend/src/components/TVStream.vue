b444bbef08ed492a3790a6555cc37060b21c07b6
Antonio Salvati <antonio.salvati@skillbill.it>
2021-05-28 12:57:52 +0200

    browser play/pause

--- frontend/src/components/TVStream.vue
+++ frontend/src/components/TVStream.vue
@@ -4,6 +4,7 @@
 
 <script>
 import shaka from "shaka-player";
+import { isBrowser } from "../lib/platformCheck";
 
 export default {
   mounted() {
@@ -14,7 +15,19 @@ export default {
 
     player.load('https://storage.googleapis.com/shaka-demo-assets/sintel/dash.mpd');
 
-    video.play().catch(console.error);
+    if(isBrowser()) {
+      document.addEventListener("click", () => {
+        if(video.paused) {
+          console.log("play");
+          video.play().catch(console.error);
+        } else {
+          console.log("pause");
+          video.pause();
+        }
+      });
+    }
+
+    window.video = video;
   }
 }
 </script>
