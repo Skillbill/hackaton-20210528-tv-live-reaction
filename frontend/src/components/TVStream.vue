a8d773b739013ae5948af55216bc2ab35afff73b
Antonio Salvati <antonio.salvati@skillbill.it>
2021-06-03 13:19:13 +0200

    play click on browsers

--- frontend/src/components/TVStream.vue
+++ frontend/src/components/TVStream.vue
@@ -1,5 +1,8 @@
 <template>
-  <video autoplay loop ref="video"></video>
+  <video loop ref="video"></video>
+  <div class="click-message" v-if="clickMessage">
+    <span>click to play the video</span>
+  </div>
 </template>
 
 <script>
@@ -7,28 +10,67 @@ import shaka from "shaka-player";
 import { isBrowser } from "../lib/platformCheck";
 
 export default {
+  data() {
+    return {
+      clickMessage: false,
+    };
+  },
+
   mounted() {
     shaka.polyfill.installAll();
 
     const video = this.$refs.video;
     const player = new shaka.Player(video);
 
-    player.load('https://storage.googleapis.com/shaka-demo-assets/sintel/dash.mpd');
+    player.load(
+      "https://storage.googleapis.com/shaka-demo-assets/sintel/dash.mpd"
+    );
 
-    if(isBrowser()) {
+    video.addEventListener("loadedmetadata", () => {
+      video.play().catch((e) => {
+        console.error(e);
+        this.clickMessage = true;
+      });
+    });
+
+    video.addEventListener("play", () => {
+      this.clickMessage = false;
+    });
+
+    if (isBrowser()) {
       document.addEventListener("click", () => {
-        if(video.paused) {
+        if (video.paused) {
           video.play().catch(console.error);
         }
       });
     }
-  }
-}
+  },
+};
 </script>
 
-<style>
-video {
+<style scooped lang="scss">
+video,
+.click-message {
+  position: absolute;
+  top: 0;
+  left: 0;
   height: 100%;
   width: 100%;
+}
+
+.click-message {
+  background-image: url("../assets/play.svg");
+  background-position: center;
+  background-size: contain;
+  background-repeat: no-repeat;
+
+  span {
+    color: white;
+    position: absolute;
+    bottom: 30%;
+    left: 0;
+    right: 0;
+    text-align: center;
+  }
 }
 </style>
